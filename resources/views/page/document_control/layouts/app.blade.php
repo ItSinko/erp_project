49180521b80d26f20c510b99aa82316853e8bc44
Ari Wijaya <ariwijaya.its@gmail.com>
2021-05-05 08:43:39 +0700

    minor change

--- resources/views/page/document_control/layouts/app.blade.php
+++ resources/views/page/document_control/layouts/app.blade.php
@@ -5,17 +5,17 @@
     <meta charset="UTF-8">
     <title>@yield('title') | {{config('settings.system_title')}}</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css">
-    <link rel="stylesheet" href="{{asset('css/lte/AdminLTE.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/lte/skins/skin-blue-light.min.css')}}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/_all.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-    <link rel="stylesheet" href="{{asset('vendor/bootstrap-wysihtml5/css/bootstrap3-wysihtml5.min.css')}}">
-    <link rel="stylesheet" href="{{asset('vendor/bootstrap-tagsinput/css/bootstrap-tagsinput.css')}}">
-    <link rel="stylesheet" href="{{asset('css/digidocu-custom.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/digidocu/css/bootstrap3.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/digidocu/css/bootstrap-toggle.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/fontawesome-free/css/all.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/select2/css/select2.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/digidocu/css/AdminLTE.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/digidocu/css/skin-blue-light.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/digidocu/css/square_all.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/daterangepicker/css/daterangepicker.css')}}" />
+    <link rel="stylesheet" href="{{asset('vendor/digidocu/css/bootstrap3-wysihtml5.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/digidocu/css/bootstrap-tagsinput.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/digidocu/css/digidocu-custom.css')}}">
     @yield('css')
 </head>
 
@@ -26,14 +26,15 @@
 
             <!-- Logo -->
             <a href="{{route('home')}}" class="hidden-xs logo">
-                <span class="logo-mini"><b>{{config('settings.system_title')[0]}}</b></span>
-                <span class="logo-lg"><b>{{config('settings.system_title')}}</b></span>
+                <span class="logo-mini"><b>D</b></span>
+                <span class="logo-lg"><b>Dokumen SPA</b></span>
             </a>
 
             <!-- Header Navbar -->
             <nav class="navbar navbar-static-top" role="navigation">
                 <!-- Sidebar toggle button-->
-                <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button" style="content: '\f0c9';">
+                    <i class="fas fa-bars"></i>
                     <span class="sr-only">Toggle navigation</span>
                 </a>
 
@@ -90,17 +91,17 @@
         </div>
     </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="{{asset('vendor/jquery/jquery.js')}}"></script>
+    <script src="{{asset('vendor/daterangepicker/js/moment.js')}}"></script>
+    <script src="{{asset('vendor/bootstrap/js/bootstrap.js')}}"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
     <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.3/js/adminlte.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.min.js"></script>
-    <script src="{{asset('vendor/bootstrap-typeahead/js/bootstrap3-typeahead.min.js')}}"></script>
-    <script src="{{asset('vendor/bootstrap-tagsinput/js/bootstrap-tagsinput.min.js')}}"></script>
-    <script src="{{asset('vendor/bootstrap-wysihtml5/js/bootstrap3-wysihtml5.all.min.js')}}"></script>
+    <script src="{{asset('vendor/digidocu/js/bootstrap3-typeahead.min.js')}}"></script>
+    <script src="{{asset('vendor/digidocu/js/bootstrap-tagsinput.min.js')}}"></script>
+    <script src="{{asset('vendor/digidocu/js/bootstrap3-wysihtml5.all.min.js')}}"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.4.2/handlebars.min.js"></script>
     <script src="{{asset('js/handlebar-helpers.js')}}"></script>
