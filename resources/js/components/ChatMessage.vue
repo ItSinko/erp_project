3fbe43bafae1ef77a2e3c6caa8d207979d0784fc
Ari Wjaya <ariwijaya.its@gmail.com>
2021-09-08 09:56:10 +0700

    make vue components

---
+++ resources/js/components/ChatMessage.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul class="chat">
+    <li class="lefy clearfix" v-for="message in messages" :key="message.id">
+      <div class="header">
+        <strong class="primary-font">
+          {{ message.user.name }}
+        </strong>
+      </div>
+      <p>
+        {{ message.message }}
+      </p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["message"],
+};
+</script>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -42,3 +42,7 @@
     </div>
 </div>
 @stop
+
+@section('adminlte_js')
+
+@stop
